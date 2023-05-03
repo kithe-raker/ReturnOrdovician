7b076186de237d761e09f987d9a700b55bf05b73
beedieonthemoon <124361777+beedieonthemoon@users.noreply.github.com>
2023-05-03 20:30:10 +0700

    add: first 3 weapons choice. also change gun name cause spacebar error.

---
+++ Assets/Code/Scripts/Weapons/First3WeaponsChoice.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class First3WeaponsChoice : MonoBehaviour
+{
+
+
+    public List<GameObject> item;
+    int numOfItems;
+    public List<Transform> point;
+    int numOfPoint;
+
+    GameObject CompactSmg;
+    GameObject Pistol;
+    GameObject Revolver;
+
+    // Start is called before the first frame update 
+    void Start()
+    {
+        numOfItems = item.Count;
+        numOfPoint = point.Count;
+    }
+    public void spawnItem(){
+        for(int i = 0;i<numOfPoint;i++){
+            Instantiate(item[i], point[i].position, transform.rotation);
+        }
+        CompactSmg = GameObject.Find("Pickup_CompactPlasmaSMG(Clone)");
+        Pistol = GameObject.Find("Pickup_PlasmaPistol(Clone)");
+        Revolver = GameObject.Find("Pickup_PlasmaRevolver(Clone)");
+    }
+
+     int once = 1;
+     
+    void Update()
+    {
+        if(once == 1){
+            spawnItem();
+            once = 2;
+        }
+
+        if((CompactSmg==null||Pistol==null||Revolver==null)&&once==2){
+            Destroy(CompactSmg);
+            Destroy(Pistol);
+            Destroy(Revolver);
+            once = 3;
+        }
+     
+    }
+}
