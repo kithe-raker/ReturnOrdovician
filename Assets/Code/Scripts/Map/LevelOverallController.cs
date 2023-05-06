59dba3d62b0040de3d631eb56b03c0fe3f283b9c
Kithe <k.rakkwaen@gmail.com>
2023-05-06 16:14:03 +0700

    edit: remove testing code

--- Assets/Code/Scripts/Map/LevelOverallController.cs
+++ Assets/Code/Scripts/Map/LevelOverallController.cs
@@ -12,7 +12,6 @@ public class LevelOverallController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        SetExitLevelDoorStatus(0, true);
     }
 
     // Update is called once per frame
