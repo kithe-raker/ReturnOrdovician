1556db22de6210acd658815cff8e4303d44f1a16
Kithe <k.rakkwaen@gmail.com>
2023-05-06 11:14:38 +0700

    refactor: implement `SlideDoorController`

--- Assets/Code/Scripts/Map/LevelOverallController.cs
+++ Assets/Code/Scripts/Map/LevelOverallController.cs
@@ -4,57 +4,28 @@ using UnityEngine;
 using UnityEngine.ProBuilder.Shapes;
 
 
-[System.Serializable]
-public class DoorStruct
-{
-    public GameObject left;
-    public GameObject right;
-    public bool isOpen;
-    public float currentRange;
-    public float openRange;
-}
-
 public class LevelOverallController : MonoBehaviour
 {
-    public List<DoorStruct> doors;
-    private const float openDoorSpeed = .5f;
+    public List<SlideDoorController> doors;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        OpenDoor(0);
+        SetDoorStatus(0, true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        UpdateDoor();
     }
-
-    public void OpenDoor(int doorIndex)
+    
+    public void SetDoorStatus(int doorIndex, bool open)
     {
         if (doorIndex < doors.Count)
         {
-            DoorStruct door = doors[doorIndex];
-            door.isOpen = true;
-            doors[doorIndex] = door;
+            doors[doorIndex].SetDoor(open);
         }
     }
 
-    void UpdateDoor()
-    {
-        for (int i = 0; i < doors.Count; i++)
-        {
-            DoorStruct door = doors[i];
-            if (door.isOpen && door.currentRange < door.openRange)
-            {
-                door.left.transform.Translate(openDoorSpeed * Time.deltaTime * Vector3.right);
-                door.right.transform.Translate(openDoorSpeed * Time.deltaTime * Vector3.left);
-                door.currentRange += 5 * Time.deltaTime; ;
-
-                doors[i] = door;
-            }
-        }
-    }
 }
