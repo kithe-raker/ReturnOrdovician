cd6320b03a32689e327c0e9e9569e1646f08a6b5
Kithe <k.rakkwaen@gmail.com>
2023-05-09 15:41:08 +0700

    edit: add `NextRoomTotalWave` for set wave in door panel

--- Assets/Code/Scripts/InteractiveObject/DoorPanels/EnterLevelPanelScript.cs
+++ Assets/Code/Scripts/InteractiveObject/DoorPanels/EnterLevelPanelScript.cs
@@ -7,6 +7,9 @@ public class EnterLevelPanelScript : MonoBehaviour
     public SpawnEnemy01 point1;
     public SpawnEnemy02 point2;
     public SpawnEnemy03 point3;
+    public int NextRoomTotalWave1 = -1;
+    public int NextRoomTotalWave2 = -1;
+    public int NextRoomTotalWave3 = -1;
     public LevelRoomObject previouseRoom;
 
     private DoorPanelController panelController;
@@ -35,6 +38,18 @@ public class EnterLevelPanelScript : MonoBehaviour
         point1.CurrentWave = 0;
         point2.CurrentWave = 0;
         point3.CurrentWave = 0;
+        if (NextRoomTotalWave1 > -1)
+        {
+            point1.Waves = NextRoomTotalWave1;
+        }
+        if (NextRoomTotalWave2 > -1)
+        {
+            point2.Waves = NextRoomTotalWave2;
+        }
+        if (NextRoomTotalWave3 > -1)
+        {
+            point3.Waves = NextRoomTotalWave3;
+        }
         if (previouseRoom)
         {
             previouseRoom.SetExitDoors(false);
