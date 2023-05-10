d71eacce9419f79883eb7933902aa00f4d7e1f75
beedieonthemoon <124361777+beedieonthemoon@users.noreply.github.com>
2023-05-11 02:05:15 +0700

    add: cheese minigun in main

--- Assets/Code/Scripts/Weapons/SpawnMiniGun.cs
+++ Assets/Code/Scripts/Weapons/SpawnMiniGun.cs
@@ -10,7 +10,7 @@ public class SpawnMiniGun : MonoBehaviour
     public InteractableObject interaction;
     public GameObject miniGun;
     public Transform point;
-
+    int once = 0;
     void Awake()
     {
         if (interaction != null)
@@ -29,6 +29,9 @@ public class SpawnMiniGun : MonoBehaviour
 
     void OnInteracted()
     {
+        if(once == 0){
         Instantiate(miniGun, point.position, transform.rotation);
+        once = 1;
+        }
     }
 }
